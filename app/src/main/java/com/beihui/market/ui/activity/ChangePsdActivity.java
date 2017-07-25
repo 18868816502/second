e7fe39b925d9050f31e32b7862bc3222c8e5230c
youlinggang <laxus0714me@gmail.com>
2017-07-25 10:47:34 +0800

    添加ChangePsdPresenter;

--- app/src/main/java/com/beihui/market/ui/activity/ChangePsdActivity.java
+++ app/src/main/java/com/beihui/market/ui/activity/ChangePsdActivity.java
@@ -2,16 +2,27 @@ package com.beihui.market.ui.activity;
 
 
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.beihui.market.R;
 import com.beihui.market.base.BaseComponentActivity;
 import com.beihui.market.injection.component.AppComponent;
+import com.beihui.market.injection.component.DaggerChangePsdComponent;
+import com.beihui.market.injection.module.ChangePsdModule;
+import com.beihui.market.ui.contract.ChangePsdContract;
+import com.beihui.market.ui.presenter.ChangePsdPresenter;
+import com.beihui.market.util.LegalInputUtils;
+import com.beihui.market.util.viewutils.ToastUtils;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class ChangePsdActivity extends BaseComponentActivity {
+public class ChangePsdActivity extends BaseComponentActivity implements ChangePsdContract.View {
     @BindView(R.id.tool_bar)
     Toolbar toolbar;
     @BindView(R.id.origin_psd)
@@ -20,6 +31,11 @@ public class ChangePsdActivity extends BaseComponentActivity {
     EditText newPsdEt;
     @BindView(R.id.new_psd_confirm)
     EditText newPsdConfirmEt;
+    @BindView(R.id.confirm)
+    Button confirmBtn;
+
+    @Inject
+    ChangePsdPresenter presenter;
 
 
     @Override
@@ -30,6 +46,30 @@ public class ChangePsdActivity extends BaseComponentActivity {
     @Override
     public void configViews() {
         setupToolbar(toolbar);
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String origin = originPsdEt.getText().toString();
+                String newPsd = newPsdEt.getText().toString();
+                String confirm = newPsdConfirmEt.getText().toString();
+
+                confirmBtn.setEnabled(LegalInputUtils.validatePassword(origin) && LegalInputUtils.validatePassword(newPsd) &&
+                        LegalInputUtils.validatePassword(confirm));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        originPsdEt.addTextChangedListener(textWatcher);
+        newPsdEt.addTextChangedListener(textWatcher);
+        newPsdConfirmEt.addTextChangedListener(textWatcher);
     }
 
     @Override
@@ -39,11 +79,30 @@ public class ChangePsdActivity extends BaseComponentActivity {
 
     @Override
     protected void configureComponent(AppComponent appComponent) {
-
+        DaggerChangePsdComponent.builder()
+                .appComponent(appComponent)
+                .changePsdModule(new ChangePsdModule(this))
+                .build()
+                .inject(this);
     }
 
     @OnClick(R.id.confirm)
-    void onConfirmClicked() {
+    void onViewClicked() {
+        presenter.updatePsd(originPsdEt.getText().toString(), newPsdEt.getText().toString(), newPsdConfirmEt.getText().toString());
+    }
 
+    @Override
+    public void setPresenter(ChangePsdContract.Presenter presenter) {
+        //injected.nothing to do.
+    }
+
+    @Override
+    public void showErrorMsg(String msg) {
+        ToastUtils.showShort(this, msg, null);
+    }
+
+    @Override
+    public void showUpdateSuccess() {
+        ToastUtils.showShort(this, "密码修改成功", null);
     }
 }
---
+++ app/src/main/java/com/beihui/market/ui/presenter/ChangePsdPresenter.java
@@ -0,0 +1,64 @@
+package com.beihui.market.ui.presenter;
+
+
+import android.content.Context;
+
+import com.beihui.market.api.Api;
+import com.beihui.market.api.ResultEntity;
+import com.beihui.market.base.BaseRxPresenter;
+import com.beihui.market.helper.UserHelper;
+import com.beihui.market.ui.contract.ChangePsdContract;
+import com.beihui.market.util.update.RxUtil;
+
+import javax.inject.Inject;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
+public class ChangePsdPresenter extends BaseRxPresenter implements ChangePsdContract.Presenter {
+
+    private Api mApi;
+    private ChangePsdContract.View mView;
+    private UserHelper.Profile profile;
+
+    @Inject
+    ChangePsdPresenter(Api api, ChangePsdContract.View view, Context context) {
+        mApi = api;
+        mView = view;
+        profile = UserHelper.getInstance(context).getProfile();
+    }
+
+    @Override
+    public void updatePsd(String origin, String newPsd, String confirm) {
+        if (origin != null && newPsd != null && confirm != null) {
+            if (!newPsd.equals(confirm)) {
+                mView.showErrorMsg("两次输入的密码不一致");
+            }
+
+            Disposable dis = mApi.updatePwd(profile.getAccount(), newPsd, origin)
+                    .compose(RxUtil.<ResultEntity>io2main())
+                    .subscribe(new Consumer<ResultEntity>() {
+                                   @Override
+                                   public void accept(@NonNull ResultEntity result) throws Exception {
+                                       if (result.isSuccess()) {
+                                           mView.showUpdateSuccess();
+                                       } else {
+                                           mView.showErrorMsg(result.getMsg());
+                                       }
+                                   }
+                               },
+                            new Consumer<Throwable>() {
+                                @Override
+                                public void accept(@NonNull Throwable throwable) throws Exception {
+                                    logError(ChangePsdPresenter.this, throwable);
+                                    mView.showErrorMsg(generateErrorMsg(throwable));
+                                }
+                            });
+            addDisposable(dis);
+
+        } else {
+            mView.showErrorMsg("输入错误");
+        }
+    }
+}
