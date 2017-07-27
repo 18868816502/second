6f51ce8dff8c912c81503812f5894d15fde7113e
youlinggang <laxus0714me@gmail.com>
2017-07-27 10:29:11 +0800

    添加AdDialog;

---
+++ app/src/main/java/com/beihui/market/ui/dialog/AdDialog.java
@@ -0,0 +1,53 @@
+package com.beihui.market.ui.dialog;
+
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.beihui.market.R;
+
+public class AdDialog extends DialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_ad, container, false);
+        view.findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getDialog().setCanceledOnTouchOutside(false);
+        Window window = getDialog().getWindow();
+        if (window != null) {
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            WindowManager.LayoutParams lp = window.getAttributes();
+            lp.y = (int) (getResources().getDisplayMetrics().density * 105);
+            window.setAttributes(lp);
+
+            window.setGravity(Gravity.TOP);
+        }
+    }
+}
