be1322b7c7866526b5caf2a443eb96e3938ffb6d
youlinggang <laxus0714me@gmail.com>
2017-08-03 17:42:51 +0800

    配置build.gradle

--- app/src/main/java/com/beihui/market/api/NetConstants.java
+++ app/src/main/java/com/beihui/market/api/NetConstants.java
@@ -1,9 +1,11 @@
 package com.beihui.market.api;
 
 
+import com.beihui.market.BuildConfig;
+
 public class NetConstants {
 
-    public static final String DOMAIN = "http://116.62.113.207:8082";
+    public static final String DOMAIN = BuildConfig.DOMAIN;
 
     public static final String BASE_PATH = "/s1";
 
@@ -14,7 +16,7 @@ public class NetConstants {
     public static final String ACCESS_KEY = "699b9305418757ef9a26e5a32ca9dbfb";
 
     /**********H5 static field********/
-    public static final String H5_DOMAIN = "http://192.168.1.34:99";
+    public static final String H5_DOMAIN = BuildConfig.H5_DOMAIN;
 
     public static final String H5_ADVICE = H5_DOMAIN + "/advice?isApp=1";
 
@@ -22,7 +24,7 @@ public class NetConstants {
 
     public static final String H5_TEST = H5_DOMAIN + "/test?isApp=1";
 
-    public static final String H5_NEWS_DETAIL = H5_DOMAIN + "/newsDetail";
+    public static final String H5_NEWS_DETAIL = H5_DOMAIN + "/newsDetail.html";
 
     public static String generateNewsUrl(String id) {
         return H5_NEWS_DETAIL + "?id=" + id + "&isApp=1";
