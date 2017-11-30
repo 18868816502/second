5a6bb5200ac12797d0658187fd10118df9964fea
youlinggang <laxus0714me@gmail.com>
2017-11-30 15:01:35 +0800

    配置xiaoneng chat key；

--- app/src/main/java/com/beihui/market/base/Constant.java
+++ app/src/main/java/com/beihui/market/base/Constant.java
@@ -5,10 +5,10 @@ public class Constant {
 
     /***********************************小能客服**********************************/
     //企业id，即企业唯一标识。格式示例：kf_9979【必填】
-    public static final String XN_SITE_ID = "kf_9032";
+    public static final String XN_SITE_ID = "kf_10075";
     //企业key，即小能通行密钥【必填】
-    public static final String XN_SITE_KEY = "6ADD069B-CB2F-4D9A-8530-C4213B2E5EAF";
+    public static final String XN_SITE_KEY = "61F24C05-0F3A-411E-81EF-4A129848E179";
     //客服组id
-    public static final String XN_CUSTOMER = "kf_9032_1499233041492";
+    public static final String XN_CUSTOMER = "kf_10075_1511865195948";
     /***************************************************************************************************/
 }
