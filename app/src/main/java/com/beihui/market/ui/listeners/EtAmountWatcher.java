2cd7a20917aabf56febd123e8a1c78665aec3189
youlinggang <laxus0714me@gmail.com>
2018-04-12 20:01:47 +0800

    修复金额上限错误；

--- app/src/main/java/com/beihui/market/ui/listeners/EtAmountWatcher.java
+++ app/src/main/java/com/beihui/market/ui/listeners/EtAmountWatcher.java
@@ -25,7 +25,7 @@ public class EtAmountWatcher implements TextWatcher {
         if (editText.getText().length() > 0) {
             try {
                 double amount = Double.parseDouble(editText.getText().toString());
-                if (amount >= 10000000) {
+                if (amount > 10000000) {
                     ToastUtils.showShort(editText.getContext(), "金额不能超过1000万", null);
                     editText.getEditableText().delete(editText.length() - 1, editText.length());
                 }
