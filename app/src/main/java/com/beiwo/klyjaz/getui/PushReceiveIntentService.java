b93633e1cd56e7fa7b9a065504f2188f3a268a80
tang <tj@kaolabill.com>
2018-10-22 13:32:45 +0800

    1022 getui cid没问题

--- app/src/main/java/com/beiwo/klyjaz/getui/PushReceiveIntentService.java
+++ app/src/main/java/com/beiwo/klyjaz/getui/PushReceiveIntentService.java
@@ -3,6 +3,7 @@ package com.beiwo.klyjaz.getui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 
 import com.beiwo.klyjaz.App;
 import com.beiwo.klyjaz.api.Api;
@@ -25,21 +26,25 @@ public class PushReceiveIntentService extends GTIntentService {
     }
 
     @Override
-    public void onReceiveClientId(Context context, String s) {
-        if (UserHelper.getInstance(App.getInstance()).getProfile() != null) {
+    public void onReceiveClientId(Context context, String clientId) {
+        if (TextUtils.isEmpty(clientId)) return;
+        //System.out.println("clientId : " + clientId);
+        if (UserHelper.getInstance(App.getInstance()).isLogin()) {
             String bindClientId = SPUtils.getPushBindClientId(context);
             String bindUserId = SPUtils.getPushBindUserId(context);
-            String userId = UserHelper.getInstance(App.getInstance()).getProfile().getId();
+            String userId = UserHelper.getInstance(context).id();
             //如果相同的userId，个推clientId已经绑定过，则不要再发送请求
-            if (!userId.equals(bindUserId) || !s.equals(bindClientId)) {
+            if (!userId.equals(bindUserId) || !clientId.equals(bindClientId)) {
                 //记录相关id
-                SPUtils.setPushBindClientId(context, s);
+                SPUtils.setPushBindClientId(context, clientId);
                 SPUtils.setPushBindUserId(context, userId);
-                Api.getInstance().bindClientId(userId, s)
+                System.out.println(userId + " === " + clientId);
+                Api.getInstance().bindClientId(userId, clientId)
                         .compose(RxResponse.compatO())
                         .subscribe(new ApiObserver<Object>() {
                             @Override
                             public void onNext(@NonNull Object data) {
+                                //System.out.println("bind getui success");
                             }
                         });
             }
@@ -51,14 +56,13 @@ public class PushReceiveIntentService extends GTIntentService {
         try {
             String json = new String(gtTransmitMessage.getPayload());
             JSONObject obj = new JSONObject(json);
-            String title = obj.getString("title") != null ? obj.getString("title") : "";
-            String content = obj.getString("content") != null ? obj.getString("content") : "";
+            String title = obj.optString("title") != null ? obj.optString("title") : "";
+            String content = obj.optString("content") != null ? obj.optString("content") : "";
 
             Intent intent = new Intent("send_push_clicked_event");
             intent.putExtra("pending_json", json);
             NotificationUtil.showNotification(context, title, content, intent, context.getPackageName() + ".push_message", false);
         } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
