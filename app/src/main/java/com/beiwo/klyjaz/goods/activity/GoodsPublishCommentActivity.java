2cac1832c6bdca7e58e8062f6cc28c9b4cde49f5
chenguoguo <cgg@kaolabill.com>
2018-12-11 11:27:55 +0800

    1211 考拉有借产品列表和评论UI创建

---
+++ app/src/main/java/com/beiwo/klyjaz/goods/activity/GoodsPublishCommentActivity.java
@@ -0,0 +1,28 @@
+package com.beiwo.klyjaz.goods.activity;
+
+import com.beiwo.klyjaz.R;
+import com.beiwo.klyjaz.base.BaseComponentActivity;
+
+/**
+ * @author chenguoguo
+ * @name loanmarket
+ * @class name：com.beiwo.klyjaz.goods.activity
+ * @descripe 产品发布评论页面
+ * @time 2018/12/11 11:19
+ */
+public class GoodsPublishCommentActivity extends BaseComponentActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_goods_publish_comment;
+    }
+
+    @Override
+    public void configViews() {
+
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+}
